044eb9ca81b0868f6a8e02244aa435540a37b0d6
Gary Marx <gary@zenpayroll.com>
2016-02-19 15:55:00 -0800

    added another rspec test

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,8 +1,10 @@
 require_relative '../models/address_book'
 
 RSpec.describe AddressBook do
+  # setup for common parameters through tests
+  let(:book) { AddressBook.new }
+
   describe 'attributes' do
-    let(:book) { AddressBook.new }
     it 'should respond to entries' do
       expect(book).to respond_to(:entries)
     end
@@ -17,7 +19,10 @@ RSpec.describe AddressBook do
   end
 
   describe '#add_entry' do
-    let(:book) { AddressBook.new.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com') }
+    before :each do
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+    end
+
     it 'adds only one entry to the address book' do
       expect(book.entries.size).to eq(1)
     end
@@ -32,13 +37,27 @@ RSpec.describe AddressBook do
   end
 
   describe '#remove_entry' do
-    let(:book) { AddressBook.new }
+    before :each do
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+    end
 
     it 'removes the desired entry' do
-      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       deleted_entry = book.remove_entry('Ada Lovelace')
-      puts "deted entry is type #{deleted_entry.class}"
       expect(book.entries.size).to eq(0)
+    end
+
+    it 'removes when there multple entries' do
+      # setup
+      book.add_entry('Alfred Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Jon Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Jim Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+
+      expect(book.entries.size).to eq(4)
+      # do it
+      deleted_entry = book.remove_entry('Jon Lovelace')
+
+      # check results
+      expect(book.entries.size).to eq(3)
     end
   end
 end
