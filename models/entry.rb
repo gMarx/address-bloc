5132c7c7acc26e605c89fe177aaa31ea9aad5197
Gary Marx <gary@zenpayroll.com>
2016-02-18 23:58:15 -0800

    added args to test & initialization in class to pass

--- models/entry.rb
+++ models/entry.rb
@@ -1,7 +1,7 @@
 class Entry
 
-  attr_accessor :name, :phone_number, :email
-  # def initialize(name, phone_number, email)
-  #   @name, @phone_number, @email = name, phone_number, email
-  # end
+  attr_reader :name, :phone_number, :email
+  def initialize(name, phone_number, email)
+    # @name, @phone_number, @email = name, phone_number, email
+  end
 end
--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -4,17 +4,17 @@ RSpec.describe Entry do
 
   describe 'attributes' do
     it 'should respond to name' do
-      entry = Entry.new
+      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expect(entry).to respond_to(:name)
     end
 
     it 'should respond to phone number' do
-      entry = Entry.new
+      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expect(entry).to respond_to(:phone_number)
     end
 
     it 'should respond to email' do
-      entry = Entry.new
+      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expect(entry).to respond_to(:email)
     end
 
