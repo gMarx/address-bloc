67e76079a766710ffc4d606ea6f39edfc30820a6
Gary Marx <gary@zenpayroll.com>
2016-02-19 21:47:25 -0800

    checkpoint 21 finished

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -4,6 +4,12 @@ RSpec.describe AddressBook do
   # setup for common parameters through tests
   let(:book) { AddressBook.new }
 
+  def check_entry(entry, expected_name, expected_number, expected_email)
+    expect(entry.name).to eq expected_name
+    expect(entry.phone_number).to eq expected_number
+    expect(entry.email).to eq expected_email
+  end
+
   describe 'attributes' do
     it 'should respond to entries' do
       expect(book).to respond_to(:entries)
@@ -61,55 +67,48 @@ RSpec.describe AddressBook do
     end
   end
 
-  def check_entry(entry, expected_name, expected_number, expected_email)
-    expect(entry.name).to eq expected_name
-    expect(entry.phone_number).to eq expected_number
-    expect(entry.email).to eq expected_email
-  end
+
 
   describe '#import_from_csv' do
-    before :each do
-      let(:populated_book) { book.import_from_csv('entries.csv') }
-    #   define entries.csv here, unless I can do a let
-    end
 
     it 'imports the correct number of entries' do
-      book_size = populated_book.entries.size
+      book.import_from_csv('entries.csv')
+      book_size = book.entries.size
 
       expect(book_size).to eq 5
     end
 
     it 'imports the first entry' do
-      # book.import_from_csv('entries.csv')
-      entry_one = populated_book.entries[0]
+      book.import_from_csv('entries.csv')
+      entry_one = book.entries[0]
 
       check_entry(entry_one, 'Bill', '555-555-4854', 'bill@blocmail.com')
     end
 
     it 'imports the second entry' do
-      # book.import_from_csv('entries.csv')
-      entry_two = populated_book.entries[1]
+      book.import_from_csv('entries.csv')
+      entry_two = book.entries[1]
 
       check_entry(entry_two, 'Bob', '555-555-5415', 'bob@blocmail.com')
     end
 
     it 'imports the 3rd entry' do
-      # book.import_from_csv('entries.csv')
-      entry_three = populated_book.entries[3]
+      book.import_from_csv('entries.csv')
+      entry_three = book.entries[2]
 
       check_entry(entry_three, 'Joe', '555-555-3660', 'joe@blocmail.com')
     end
 
     it 'imports the 4th entry' do
-      # book.import_from_csv('entires.csv')
-      entry_four = populated_book.entries[4]
+      book.import_from_csv('entries.csv')
+      entry_four = book.entries[3]
 
       check_entry(entry_four, 'Sally', '555-555-4646', 'sally@blocmail.com')
     end
 
     it 'imports the 5th entry' do
-      # book.import_from_csv('entries.csv')
-      entry_five = populated_book.entries[5]
+      book.import_from_csv('entries.csv')
+      entry_five = book.entries[4]
 
       check_entry(entry_five, 'Sussie', '555-555-2036', 'sussie@blocmail.com')
     end
